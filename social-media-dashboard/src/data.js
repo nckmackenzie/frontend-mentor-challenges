8520c510fb13542f236f8b50fb3b22ba8da03452
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-05 16:50:27 +0300

    followers array

---
+++ social-media-dashboard/src/data.js
@@ -0,0 +1,30 @@
+export const followers = [
+  {
+    social: 'twitter',
+    followers: '1987',
+    increase: 12,
+    bg: 'hsl(203, 89%, 53%)',
+    imgsrc: './assets/icon-twitter.svg',
+  },
+  {
+    social: 'facebook',
+    followers: '1044',
+    increase: 99,
+    bg: 'hsl(208, 92%, 53%)',
+    imgsrc: './assets/icon-facebook.svg',
+  },
+  {
+    social: 'instagram',
+    followers: '11k',
+    increase: 1099,
+    bg: 'hsl(37, 97%, 70%)',
+    imgsrc: './assets/icon-instagram.svg',
+  },
+  {
+    social: 'youtube',
+    followers: '8239',
+    increase: 144,
+    bg: 'hsl(348, 97%, 39%)',
+    imgsrc: './assets/icon-youtube.svg',
+  },
+];
