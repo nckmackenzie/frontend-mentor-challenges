6cfaa9ac69ccef8146e683574c2f954e3c21553d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-05 21:22:18 +0300

    set min height

--- social-media-dashboard/src/App.jsx
+++ social-media-dashboard/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from './components/layout/Header';
+import Overview from './components/overview/Overview';
 import { useTheme } from './context/ThemeProvider';
 function App() {
   const { darkMode } = useTheme();
   return (
     <div
-      className={`font-inter w-full h-screen ${
+      className={`font-inter w-full min-h-screen ${
         !darkMode ? 'bg-white' : 'bg-dark-bg'
       }`}
     >
       <Header />
+      <Overview />
     </div>
   );
 }
