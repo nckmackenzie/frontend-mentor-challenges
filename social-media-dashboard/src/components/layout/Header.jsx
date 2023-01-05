63d28189997952730e6a1f22007531378e3dd62b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-05 16:47:58 +0300

    add followers cards component

--- social-media-dashboard/src/components/layout/Header.jsx
+++ social-media-dashboard/src/components/layout/Header.jsx
@@ -1,21 +1,31 @@
 import { useTheme } from '../../context/ThemeProvider';
+import Cards from '../header/Cards';
 import Container from '../ui/Container';
+import Switch from '../ui/Switch';
 function Header() {
-  const { bg, mainText, secondaryText } = useTheme();
-
+  const { bg, mainText, secondaryText, darkMode } = useTheme();
+  const lineColor = !darkMode ? 'bg-light-toggle' : 'bg-dark-card';
+  // bg-dark-card
   return (
-    <header className={`h-[25vh] pt-6 ${bg}`}>
+    <header className={`h-[30vh] md:h-[25vh] pt-6 ${bg} relative`}>
       <Container>
-        <div className="flex items-center justify-between">
-          <div>
-            <h1 className={`text-2xl font-bold capitalize mb-0 ${mainText}`}>
+        <div className="flex flex-col md:flex-row items-start md:items-center gap-4">
+          <div className="md:mr-auto">
+            <h1
+              className={`text-lg md:text-2xl font-bold capitalize mb-0 ${mainText}`}
+            >
               social media dashboard
             </h1>
-            <div className={`text-sm font-bold ${secondaryText}`}>
+            <div className={`text-xs font-bold ${secondaryText}`}>
               Total followers: 23,004
             </div>
           </div>
+          <div
+            className={`inline-flex md:hidden h-[1px] w-full ${lineColor}`}
+          ></div>
+          <Switch />
         </div>
+        <Cards />
       </Container>
     </header>
   );
