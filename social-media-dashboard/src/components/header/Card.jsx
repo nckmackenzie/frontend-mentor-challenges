d0d01498d155f3fea55edfac6ea33c13e3ea7f6e
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-05 20:24:04 +0300

    Card Component

--- social-media-dashboard/src/components/header/Card.jsx
+++ social-media-dashboard/src/components/header/Card.jsx
@@ -1,17 +1,34 @@
+import { BiCaretDown } from 'react-icons/bi';
 import { useTheme } from '../../context/ThemeProvider';
 function Card({ social, followers, increase, bg, imgsrc }) {
-  const { cardBg, secondaryText } = useTheme();
+  const { cardBg, secondaryText, mainText } = useTheme();
   return (
     <div
-      className={`shadow-md rounded-md overflow-hidden h-32 basis-full md:basis-1/4 ${cardBg}`}
+      className={`shadow-md rounded-md overflow-hidden h-52 w-full md:w-1/4 ${cardBg}`}
       style={{ borderTopWidth: '4px', borderStyle: 'solid', borderColor: bg }}
     >
-      <div className="flex items-center justify-center gap-6 h-full">
-        <div className="flex gap-1">
+      <div className="flex flex-col items-center justify-center gap-4 h-full">
+        <div className="flex gap-1 items-center">
           <img src={imgsrc} alt={social} className="h-6 w-auto" />
-          <span className={`text-[10px] font-bold ${secondaryText}`}>
-            @nathanf
+          <span className={`text-xs font-bold ${secondaryText}`}>
+            @nckmackenzie
           </span>
+        </div>
+        <div>
+          <p className={`text-6xl font-bold ${mainText}`}>{followers}</p>
+          <div
+            className={`text-base tracking-[.25em] uppercase ${secondaryText} text-center`}
+          >
+            Followers
+          </div>
+        </div>
+        <div
+          className={`flex items-center gap-1 text-sm font-bold ${
+            increase < 0 ? 'text-bright-red' : 'text-lime-green'
+          }`}
+        >
+          <BiCaretDown />
+          <span className="inline-flex">{Math.abs(increase)} Today</span>
         </div>
       </div>
     </div>
