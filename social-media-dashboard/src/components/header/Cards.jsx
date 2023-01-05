35bd0b503c4ff0cdf9f0933cb0a5191165b624e0
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-05 20:23:19 +0300

    set full with

--- social-media-dashboard/src/components/header/Cards.jsx
+++ social-media-dashboard/src/components/header/Cards.jsx
@@ -2,7 +2,7 @@ import { followers } from '../../data';
 import Card from './Card';
 function Cards() {
   return (
-    <div className="mt-4 flex flex-col md:flex-row gap-4">
+    <div className="mt-4 flex flex-col md:flex-row gap-4 absolute left-0 w-full">
       {followers &&
         followers.length > 0 &&
         followers.map(follower => <Card key={follower.social} {...follower} />)}
