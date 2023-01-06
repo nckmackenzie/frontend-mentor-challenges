d2bde7cca7b300e4f1bcfc367b6250947e69bc64
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-01-06 23:22:09 +0300

    initial setup

---
+++ static-job-listing/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}', './src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      spartan: ['League Spartan', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
