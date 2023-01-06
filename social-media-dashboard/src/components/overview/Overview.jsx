a3044825836beb659929e32f222c9f7d819e53b9
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-06 13:17:13 +0300

    finalize overview component

--- social-media-dashboard/src/components/overview/Overview.jsx
+++ social-media-dashboard/src/components/overview/Overview.jsx
@@ -1,12 +1,16 @@
 import Container from '../ui/Container';
 import { useTheme } from '../../context/ThemeProvider';
+import Stats from './Stats';
 
 function Overview() {
   const { mainText } = useTheme();
   return (
     <section className="pb-6 pt-12">
       <Container>
-        <h2 className={`${mainText} text-2xl font-bold`}>Overview - Today</h2>
+        <h2 className={`${mainText} text-2xl font-bold mb-4`}>
+          Overview - Today
+        </h2>
+        <Stats />
       </Container>
     </section>
   );
