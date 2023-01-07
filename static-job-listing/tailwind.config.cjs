1d1afe6683ee4a29791efc1779c9dda572a31f22
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-07 08:58:50 +0300

    add custom font size and background images

--- static-job-listing/tailwind.config.cjs
+++ static-job-listing/tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./**/*.{html,js}', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontSize: {
+      app: '15px',
+    },
+    extend: {
+      colors: {
+        primary: 'hsl(180, 29%, 50%)',
+        'grayish-cyan-400': 'hsl(180, 52%, 96%)',
+        'grayish-cyan-300': 'hsl(180, 31%, 95%)',
+        'grayish-cyan-600': 'hsl(180, 8%, 52%)',
+        'grayish-cyan-600': 'hsl(180, 14%, 20%)',
+      },
+      backgroundImage: {
+        'hero-pattern-desktop': 'url("/src/assets/bg-header-desktop.svg")',
+        'hero-pattern-mobile': 'url("/src/assets/bg-header-mobile.svg")',
+      },
+    },
     fontFamily: {
       spartan: ['League Spartan', 'sans-serif'],
     },
