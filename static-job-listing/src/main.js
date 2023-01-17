f1328c0026b7b28a6ba14e9293b982ab1566c8ec
nckmackenzie <nicholas.mackenzie1@gmail.com>
2023-01-17 16:49:59 +0300

    add filtered function

--- static-job-listing/src/main.js
+++ static-job-listing/src/main.js
@@ -4,15 +4,15 @@ const container = document.getElementById('app');
 
 const app = new Controller(container); //create new instance of Controller class
 //set filters object
-const filters = { roles: [], languages: [], levels: [] };
-
+// const filters = { roles: [], languages: [], levels: [] };
+const filters = [];
 //set all jobs on load
 app.setJobs(jobListings);
 
 //use event delegation to check if a 'filter-pill' is clicked
 document.querySelector('section').addEventListener('click', e => {
   if (!e.target.classList.contains('filter-pills')) return;
-  addToFilter(e.target.textContent, e.target.classList[1]);
+  addToFilter(e.target.textContent);
   let filteredJobs = getFilteredJobs();
   console.log(filteredJobs);
   console.log(filters);
@@ -32,44 +32,63 @@ document.querySelector('section').addEventListener('click', e => {
   // app.setFilters(filteredJobs);
 });
 
+//function
 function getFilteredJobs() {
-  let filteredJobs;
-  if (
-    (filters.roles.length !== 0 && filters.languages.length === 0) ||
-    filters.levels.length === 0
-  ) {
-    filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
-  }
-  if (
-    filters.roles.length !== 0 &&
-    filters.languages.length !== 0 &&
-    filters.levels.length === 0
-  ) {
-    filteredJobs = jobListings.filter(
-      job =>
-        filters.roles.includes(job.role) &&
-        filters.languages.every(language => job.languages.includes(language))
-    );
-  }
-  if (
-    filters.roles.length !== 0 &&
-    filters.languages.length !== 0 &&
-    filters.levels.length !== 0
-  ) {
-    filteredJobs = jobListings.filter(
-      job =>
-        filters.roles.includes(job.role) &&
-        filters.levels.includes(job.level) &&
-        filters.languages.every(language => job.languages.includes(language))
-    );
-  }
-
+  const filteredJobs = jobs.filter(job => {
+    let count = 0;
+    for (const elm in filters) {
+      if (
+        job.level === elm ||
+        job.languages.includes(elm) ||
+        job.role === elm
+      ) {
+        count++;
+      }
+    }
+    if (filters.length === count) {
+      return job;
+    }
+  });
   return filteredJobs;
 }
 
+// function getFilteredJobs() {
+//   let filteredJobs;
+//   if (
+//     (filters.roles.length !== 0 && filters.languages.length === 0) ||
+//     filters.levels.length === 0
+//   ) {
+//     filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
+//   }
+//   if (
+//     filters.roles.length !== 0 &&
+//     filters.languages.length !== 0 &&
+//     filters.levels.length === 0
+//   ) {
+//     filteredJobs = jobListings.filter(
+//       job =>
+//         filters.roles.includes(job.role) &&
+//         filters.languages.every(language => job.languages.includes(language))
+//     );
+//   }
+//   if (
+//     filters.roles.length !== 0 &&
+//     filters.languages.length !== 0 &&
+//     filters.levels.length !== 0
+//   ) {
+//     filteredJobs = jobListings.filter(
+//       job =>
+//         filters.roles.includes(job.role) &&
+//         filters.levels.includes(job.level) &&
+//         filters.languages.every(language => job.languages.includes(language))
+//     );
+//   }
+
+//   return filteredJobs;
+// }
+
 //helper function
-function addToFilter(value, type) {
-  const filterKey = `${type}s`;
-  if (filters[filterKey].includes(value)) return;
-  filters[filterKey].push(value);
+function addToFilter(value) {
+  if (filters.includes(value)) return;
+  filters.push(value);
 }
