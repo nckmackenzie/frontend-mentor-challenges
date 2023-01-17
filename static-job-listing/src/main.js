fa8d8d4754d5c991ed81aad981ccecd57f6c6a9b
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-01-17 06:25:48 +0300

    changes
    changes

--- static-job-listing/src/main.js
+++ static-job-listing/src/main.js
@@ -2,32 +2,74 @@ import Controller from './Controller';
 import { jobListings } from './data';
 const container = document.getElementById('app');
 
-const app = new Controller(container);
+const app = new Controller(container); //create new instance of Controller class
 //set filters object
 const filters = { roles: [], languages: [], levels: [] };
 
 //set all jobs on load
 app.setJobs(jobListings);
 
-//loop through all instances of .filter-pills
-document.querySelectorAll('.filter-pills').forEach(pill => {
-  //click event handler
-  pill.addEventListener('click', e => {
-    console.log('first');
-    addToFilter(e.target.textContent, e.target.classList[1]);
-    const filteredJobs = jobListings.filter(
+//use event delegation to check if a 'filter-pill' is clicked
+document.querySelector('section').addEventListener('click', e => {
+  if (!e.target.classList.contains('filter-pills')) return;
+  addToFilter(e.target.textContent, e.target.classList[1]);
+  let filteredJobs = getFilteredJobs();
+  console.log(filteredJobs);
+  console.log(filters);
+  // app.setFilters(filteredJobs);
+  // if (
+  //   (filters.roles.length > 0 && filters.languages.length === 0) ||
+  //   filters.levels.length === 0
+  // ) {
+  //   filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
+  // }
+  // // const filteredJobs = jobListings.filter(
+  // //   job =>
+  // //     filters.roles.includes(job.role) &&
+  // //     filters.levels.includes(job.level) &&
+  // //     filters.languages.every(language => job.languages.includes(language))
+  // // );
+  // app.setFilters(filteredJobs);
+});
+
+function getFilteredJobs() {
+  let filteredJobs;
+  if (
+    (filters.roles.length !== 0 && filters.languages.length === 0) ||
+    filters.levels.length === 0
+  ) {
+    filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
+  }
+  if (
+    filters.roles.length !== 0 &&
+    filters.languages.length !== 0 &&
+    filters.levels.length === 0
+  ) {
+    filteredJobs = jobListings.filter(
       job =>
         filters.roles.includes(job.role) &&
         filters.languages.every(language => job.languages.includes(language))
     );
-    app.setFilters(filteredJobs);
-  });
-});
+  }
+  if (
+    filters.roles.length !== 0 &&
+    filters.languages.length !== 0 &&
+    filters.levels.length !== 0
+  ) {
+    filteredJobs = jobListings.filter(
+      job =>
+        filters.roles.includes(job.role) &&
+        filters.levels.includes(job.level) &&
+        filters.languages.every(language => job.languages.includes(language))
+    );
+  }
+
+  return filteredJobs;
+}
 
 //helper function
 function addToFilter(value, type) {
   const filterKey = `${type}s`;
   if (filters[filterKey].includes(value)) return;
   filters[filterKey].push(value);
-  //   console.log(filters);
 }
