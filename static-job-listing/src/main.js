981dd08a8cca292d1e848620ea95cdc932b3411f
Nick Mackenzie <34639621+nckmackenzie@users.noreply.github.com>
2023-01-18 21:34:05 +0300

    filter and clear functionality

--- static-job-listing/src/main.js
+++ static-job-listing/src/main.js
@@ -3,8 +3,9 @@ import { jobListings } from './data';
 const container = document.getElementById('app');
 
 const app = new Controller(container); //create new instance of Controller class
+const clearBtn = document.querySelector('.clear-btn');
 //set filters object
-const filters = { roles: [], languages: [], levels: [] };
+const filters = [];
 
 //set all jobs on load
 app.setJobs(jobListings);
@@ -12,64 +13,51 @@ app.setJobs(jobListings);
 //use event delegation to check if a 'filter-pill' is clicked
 document.querySelector('section').addEventListener('click', e => {
   if (!e.target.classList.contains('filter-pills')) return;
-  addToFilter(e.target.textContent, e.target.classList[1]);
+  addToFilter(e.target.textContent);
   let filteredJobs = getFilteredJobs();
-  console.log(filteredJobs);
-  console.log(filters);
-  // app.setFilters(filteredJobs);
-  // if (
-  //   (filters.roles.length > 0 && filters.languages.length === 0) ||
-  //   filters.levels.length === 0
-  // ) {
-  //   filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
-  // }
-  // // const filteredJobs = jobListings.filter(
-  // //   job =>
-  // //     filters.roles.includes(job.role) &&
-  // //     filters.levels.includes(job.level) &&
-  // //     filters.languages.every(language => job.languages.includes(language))
-  // // );
-  // app.setFilters(filteredJobs);
+  app.setFilters(filteredJobs, filters);
+});
+
+//clear filter
+document.querySelector('.filter-box').addEventListener('click', e => {
+  if (!e.target.classList.contains('remove-filter')) return;
+  const clickedFilter = e.target.previousElementSibling.textContent;
+  const i = filters.findIndex(index => index === clickedFilter);
+  filters.splice(i, 1);
+  if (filters.length) {
+    let filteredJobs = getFilteredJobs();
+    app.setFilters(filteredJobs, filters);
+  } else {
+    app.clearFilters(jobListings);
+  }
+});
+
+//clear filters
+clearBtn.addEventListener('click', () => {
+  app.clearFilters(jobListings);
 });
 
 function getFilteredJobs() {
-  let filteredJobs;
-  if (
-    (filters.roles.length !== 0 && filters.languages.length === 0) ||
-    filters.levels.length === 0
-  ) {
-    filteredJobs = jobListings.filter(job => filters.roles.includes(job.role));
-  }
-  if (
-    filters.roles.length !== 0 &&
-    filters.languages.length !== 0 &&
-    filters.levels.length === 0
-  ) {
-    filteredJobs = jobListings.filter(
-      job =>
-        filters.roles.includes(job.role) &&
-        filters.languages.every(language => job.languages.includes(language))
-    );
-  }
-  if (
-    filters.roles.length !== 0 &&
-    filters.languages.length !== 0 &&
-    filters.levels.length !== 0
-  ) {
-    filteredJobs = jobListings.filter(
-      job =>
-        filters.roles.includes(job.role) &&
-        filters.levels.includes(job.level) &&
-        filters.languages.every(language => job.languages.includes(language))
-    );
-  }
-
+  const filteredJobs = jobListings.filter(job => {
+    let cnt = 0;
+    for (let i = 0; i < filters.length; i++) {
+      if (
+        job.languages.includes(filters[i]) ||
+        job.role === filters[i] ||
+        job.level === filters[i]
+      ) {
+        cnt++;
+      }
+    }
+    if (filters.length == cnt) {
+      return job;
+    }
+  });
   return filteredJobs;
 }
 
 //helper function
-function addToFilter(value, type) {
-  const filterKey = `${type}s`;
-  if (filters[filterKey].includes(value)) return;
-  filters[filterKey].push(value);
+function addToFilter(value) {
+  if (filters.includes(value)) return;
+  filters.push(value);
 }
